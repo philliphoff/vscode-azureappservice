86c9f4b2b21455173dff9756193f825dc76f340c
phoff <phoff@microsoft.com>
2022-03-16 15:19:16 -0700

    Update to match latest APIs.

--- src/workflows/api.ts
+++ src/workflows/api.ts
@@ -1,52 +1,52 @@
 import * as vscode from "vscode";
 
-export interface WorkflowResult<T> {
-    result?: T;
-    succeeded: boolean;
-}
-
 export interface WorkflowCreationContext {
+    /**
+     * Optional and opaque context provided by the caller when creating the workflow.
+     */
     readonly callerContext?: never;
 
-    createWorkflowFromContent(suggestedFileName: string, content: string): Promise<WorkflowResult<vscode.Uri>>;
+    /**
+     * Initial content (if any) for a new workflow. Populated, for example, when creating a starter workflow.
+     */
+    readonly content?: string;
 
     /**
-     * Sets or updates an actions secret in the GitHub repository.
+     * Initial name for a new workflow file. Populated, for example, when creating a starter workflow.
+     */
+    readonly suggestedFileName?: string;
+
+    /**
+     * Creates a new workflow file.
+     *
+     * @param suggestedFileName The suggested file name for a new workflow.
+     * @param content The content of the workflow.
+     *
+     * @returns The actual URI of the created workflow.
+     */
+    createWorkflowFile(suggestedFileName: string, content: string): Promise<vscode.Uri | undefined>;
+
+    /**
+     * Creates or updates an actions secret in the GitHub repository.
      *
      * @param suggestedName The suggested name for the secret.
-     * @param value The value of the secret.
+     * @param value The new value of the secret.
      *
-     * @returns The actual name of the created/set secret.
+     * @returns The actual name of the created/updated secret.
      */
-    setSecret(suggestedName: string, value: string): Promise<WorkflowResult<string>>;
+    setSecret(suggestedName: string, value: string): Promise<string | undefined>;
 }
 
-export interface StarterWorkflowCreationContext extends WorkflowCreationContext {
-    readonly content: string;
-}
-
-export interface WorkflowTemplateDefinition {
-    readonly description: string;
-    readonly group: { id: string, label: string};
-    readonly id: string;
-    readonly label: string;
-
-    onCreate(context: WorkflowCreationContext): Promise<void>;
-}
-
-export interface StarterWorkflowTemplateDefinition {
-    readonly id: string;
-
-    onCreate(context: StarterWorkflowCreationContext): Promise<void>;
+export interface GitHubWorkflowProvider {
+    createWorkflow(context: WorkflowCreationContext): Promise<void>;
 }
 
 export interface GitHubActionsApi {
-    createWorkflowFromTemplate(templateId: string, callerContext?: never): Promise<void>;
+    createWorkflow(type: string, callerContext?: never): Promise<vscode.Uri[]>;
 
-    registerStarterWorkflowTemplate(definition: StarterWorkflowTemplateDefinition): vscode.Disposable;
-    registerWorkflowTemplate(definition: WorkflowTemplateDefinition): vscode.Disposable;
+    registerWorkflowProvider(type: string, provider: GitHubWorkflowProvider): vscode.Disposable;
 }
 
 export interface GitHubActionsApiManager {
-    getApi<T>(version: string): T | undefined
+    getApi<T>(version: 1): T | undefined
 }
--- src/workflows/starterWorkflows.ts
+++ src/workflows/starterWorkflows.ts
@@ -10,18 +10,23 @@ export async function registerStarterWorkflowTemplates(): Promise<void> {
         const manager: GitHubActionsApiManager | undefined = gitHubActionsExtension.exports as GitHubActionsApiManager;
 
         if (manager) {
-            const api: GitHubActionsApi | undefined = manager.getApi('1.0.0');
+            const api: GitHubActionsApi | undefined = manager.getApi(1);
 
             if (api) {
-                api.registerStarterWorkflowTemplate({
-                    id: 'deployments/azure-webapps-dotnet-core',
-                    onCreate: async (context): Promise<void> => {
-                        await context.setSecret('AZURE_WEBAPP_PUBLISH_PROFILE', 'My Publish Profile');
+                api.registerWorkflowProvider(
+                    'deployments/azure-webapps-dotnet-core',
+                    {
+                        createWorkflow: async (context): Promise<void> => {
+                            if (!context.content) {
+                                throw new Error('Unable to get the starter workflow content.');
+                            }
 
-                        // Expose suggested file name.
-                        await context.createWorkflowFromContent('starter-template.yml', context.content);
-                    }
-                });
+                            await context.setSecret('AZURE_WEBAPP_PUBLISH_PROFILE', 'My Publish Profile');
+
+                            // Expose suggested file name.
+                            await context.createWorkflowFile(context.suggestedFileName ?? 'azure-webapps-dotnet-core.yml', context.content);
+                        }
+                    });
             }
         }
     }
